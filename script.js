4d079bdbc34a9c176207b576cc315216b73144de
Chaz <74632592+chazpf@users.noreply.github.com>
2021-03-11 01:15:27 -0600

    test - integrating classes

--- script.js
+++ script.js
@@ -1,16 +1,58 @@
+// ------------Classes----------
+class Card {
+    constructor(name, id) {
+        this.name = name;
+        this.id = name + id;
+    }
+
+    //method
+    discard() {
+        let index = hand.indexOf(this);
+        hand.splice(index, 1);
+        discardPile.push(this);
+        updateHand();
+        updateDiscardPile();
+    }
+}
+
+class Deck {
+    constructor(decklist) {
+        this.decklist = decklist;
+        this.stack = [];
+    }
+
+    createStack() {
+        this.decklist.forEach(([num, cardName]) => {
+            for (let i = 1; i <= num; i++) {
+                const newCard = new Card(cardName, i);
+                this.stack.push(newCard);
+            }
+        });
+    }
+
+    /* From: https://stackoverflow.com/a/12646864/14890950 */
+    shuffle() {
+        for (let i = this.stack.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.stack[i], this.stack[j]] = [this.stack[j], this.stack[i]];
+        }
+    }
+
+    draw() {
+        if (this.stack[0]) hand.push(this.stack.pop());
+        updateDeckSize();
+        updateHand();
+    }
+}
+
 // ------------Initiation----------
-const deck = [
-    'Card1',
-    'Card2',
-    'Card3',
-    'Card4',
-    'Card5',
-    'Card6',
-    'Card7',
-    'Card8',
-    'Card9',
-    'Card10',
+
+const decklist = [
+    [17, 'Land'],
+    [5, 'Spell'],
+    [18, 'Creature'],
 ];
+const deck = new Deck(decklist);
 const hand = [];
 const discardPile = [];
 
@@ -19,10 +61,11 @@ const handDisplay = document.querySelector('.hand');
 const discardPileDisplay = document.querySelector('.discard');
 
 window.onload = () => {
-    shuffleArray(deck);
+    deck.createStack();
+    deck.shuffle();
     updateDisplays();
     document.querySelector('.btn--draw').addEventListener('click', () => {
-        draw();
+        deck.draw();
     });
     document.querySelector('.btn--reset').addEventListener('click', () => {
         reset();
@@ -31,33 +74,10 @@ window.onload = () => {
 
 // ------------Controller----------
 
-/* Randomize array in-place using Durstenfeld shuffle algorithm
-https://stackoverflow.com/a/12646864/14890950 */
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-function draw() {
-    if (deck[0]) hand.push(deck.pop());
-    updateDeckSize();
-    updateHand();
-}
-
-function discard(card) {
-    let index = hand.indexOf(card);
-    const discardedCard = hand.splice(index, 1);
-    discardPile.push(...discardedCard);
-    updateHand();
-    updateDiscardPile();
-}
-
 function reset() {
-    deck.push(...hand.splice(0));
-    deck.push(...discardPile.splice(0));
-    shuffleArray(deck);
+    deck.stack.push(...hand.splice(0));
+    deck.stack.push(...discardPile.splice(0));
+    deck.shuffle();
     updateDisplays();
 }
 
@@ -69,7 +89,7 @@ function updateDisplays() {
 }
 
 function updateDeckSize() {
-    deckDisplay.innerHTML = `Deck size: ${deck.length}`;
+    deckDisplay.innerHTML = `Deck size: ${deck.stack.length}`;
 }
 
 function updateHand() {
@@ -79,11 +99,15 @@ function updateHand() {
     cleanZone(visualHand);
     // add cards in hand array that are not yet visible
     for (const card of hand)
-        if (!visualHand.namedItem(card))
+        if (!visualHand.namedItem(card.id)) {
             handDisplay.insertAdjacentHTML(
                 'beforeend',
-                `<span class="card" id="${card}" onClick="discard('${card}')">${card}</span>`
+                `<span class="card" id="${card.id}">${card.name}</span>`
             );
+            document
+                .getElementById(card.id)
+                .addEventListener('click', () => card.discard());
+        }
 }
 
 function updateDiscardPile() {
@@ -93,10 +117,10 @@ function updateDiscardPile() {
     cleanZone(visualDiscardPile);
     // add cards in discardPile array that are not yet visible
     for (const card of discardPile)
-        if (!visualDiscardPile.namedItem(card))
+        if (!visualDiscardPile.namedItem(card.id))
             discardPileDisplay.insertAdjacentHTML(
                 'beforeend',
-                `<span class="card" id="${card}">${card}</span>`
+                `<span class="card" id="${card.id}">${card.name}</span>`
             );
 }
 
